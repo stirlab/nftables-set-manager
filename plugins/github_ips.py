12696239fa70d19390760aec8cee28d620db3629
Chad Phillips <chad@apartmentlines.com>
2020-07-24 22:50:14 -0700

    allow custom json_url

--- plugins/github_ips.py
+++ plugins/github_ips.py
@@ -2,29 +2,7 @@ import json
 import requests
 from requests.exceptions import HTTPError
 
-GITHUB_IP_RANGES_JSON_URL = 'https://api.github.com/meta'
-# TODO: This sucks, but the A record of api.github.com changes so fast that it
-# cannot be trusted from a DNS query.
-API_IP_RANGES = [
-    "192.30.252.0/22",
-    "185.199.108.0/22",
-    "140.82.112.0/20",
-    "13.230.158.120/32",
-    "18.179.245.253/32",
-    "52.69.239.207/32",
-    "13.209.163.61/32",
-    "54.180.75.25/32",
-    "13.233.76.15/32",
-    "13.234.168.60/32",
-    "13.250.168.23/32",
-    "13.250.94.254/32",
-    "54.169.195.247/32",
-    "13.236.14.80/32",
-    "13.238.54.232/32",
-    "52.63.231.178/32",
-    "18.229.199.252/32",
-    "54.207.47.76/32"
-]
+GITHUB_IP_RANGES_DEFAULT_JSON_URL = 'https://api.github.com/meta'
 IP_TYPES_DEFAULT = [
     'web',
     'api',
@@ -38,6 +16,7 @@ class GetElements(object):
         self.logger = logger
         self.config = config
         self.args = args
+        self.json_url = 'json_url' in metadata and metadata['json_url'] or GITHUB_IP_RANGES_DEFAULT_JSON_URL
         self.ip_types = 'ip_types' in metadata and metadata['ip_types'] or IP_TYPES_DEFAULT
 
     def get_elements(self):
@@ -45,9 +24,9 @@ class GetElements(object):
         return github_ips
 
     def get_github_ips_for_types(self):
-        self.logger.debug("Retrieving data from : %s" % GITHUB_IP_RANGES_JSON_URL)
+        self.logger.debug("Retrieving JSON data from : %s" % self.json_url)
         try:
-            response = requests.get(GITHUB_IP_RANGES_JSON_URL)
+            response = requests.get(self.json_url)
             response.raise_for_status()
             data = response.json()
         except HTTPError as http_err:
@@ -59,9 +38,6 @@ class GetElements(object):
         return self.build_elements(data)
 
     def build_elements(self, data):
-        elements = []
-        for element in API_IP_RANGES:
-            elements.append(element)
         for ip_type in data:
             if ip_type in self.ip_types:
                 self.logger.debug("Adding elements for IP type: %s" % ip_type)
