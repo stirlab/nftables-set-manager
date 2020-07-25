93b8ccddd0e48e7fc9d9823a384323b9f8720fea
Chad Phillips <chad@apartmentlines.com>
2020-07-24 21:39:01 -0700

    add logging, default regions

--- plugins/s3_ips.py
+++ plugins/s3_ips.py
@@ -3,6 +3,9 @@ import requests
 from requests.exceptions import HTTPError
 
 S3_IP_RANGES_JSON_URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+REGIONS_DEFAULT = [
+    'us-east-1',
+]
 
 class GetElements(object):
 
@@ -11,13 +14,14 @@ class GetElements(object):
         self.logger = logger
         self.config = config
         self.args = args
-        self.regions = 'regions' in metadata and metadata['regions'] or None
+        self.regions = 'regions' in metadata and metadata['regions'] or REGIONS_DEFAULT
 
     def get_elements(self):
         s3_ips = self.get_s3_ips_for_regions()
         return s3_ips
 
     def get_s3_ips_for_regions(self):
+        self.logger.debug("Retrieving data from : %s" % S3_IP_RANGES_JSON_URL)
         try:
             response = requests.get(S3_IP_RANGES_JSON_URL)
             response.raise_for_status()
@@ -31,4 +35,5 @@ class GetElements(object):
         return self.build_elements(data)
 
     def build_elements(self, data):
+        self.logger.debug("Adding elements from regions: %s" % json.dumps(self.regions))
         return [obj['ip_prefix'] for obj in data['prefixes'] if not self.regions or obj['region'] in self.regions]
