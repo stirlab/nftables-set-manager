2a364d8b5b25afcf68139ea2173b7b047186cdf0
Chad Phillips <chad@apartmentlines.com>
2021-08-18 14:33:54 -0400

    drop slash value when checking for ipv4 address

--- plugins/github_ips.py
+++ plugins/github_ips.py
@@ -35,7 +35,7 @@ class GetElements(Plugin):
             if ip_type in self.ip_types:
                 self.logger.debug("Adding elements for IP type: %s" % ip_type)
                 for element in data[ip_type]:
-                    if self.is_ipv4_address(element):
+                    if self.is_ipv4_address(element.split("/")[0]):
                         elements.append(element)
                     else:
                         self.logger.debug("Skipping non-IPv4 address: %s" % element)
